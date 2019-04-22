e790ec3affd4c8c762c776133bfcb2d7e1e9ae3d
emirp47 <tuanlt47@gmail.com>
2019-04-22 14:52:05 +0700

    first commit

---
+++ src/main/java/dn/codegym/crm/CodegymCrmApplication.java
@@ -0,0 +1,13 @@
+package dn.codegym.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodegymCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodegymCrmApplication.class, args);
+    }
+
+}
