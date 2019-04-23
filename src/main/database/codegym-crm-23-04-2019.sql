6803d79ba8be7b944c5effe95a59db560badd9b7
zitxiem1998 <zitxiem1998@gmail.com>
2019-04-23 16:45:57 +0700

    Add sql

---
+++ src/main/database/codegym-crm-23-04-2019.sql
@@ -0,0 +1,220 @@
+-- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
+--
+-- Host: localhost    Database: codegym_crm
+-- ------------------------------------------------------
+-- Server version	8.0.15
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `campaigns`
+--
+
+DROP TABLE IF EXISTS `campaigns`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `campaigns` (
+  `id` varchar(36) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `description` longtext,
+  `created_by` varchar(255) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `last_updated_date` datetime DEFAULT NULL,
+  `last_updated_by` varchar(255) DEFAULT NULL,
+  `version` bigint(20) NOT NULL,
+  `delected` bit(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `campaigns`
+--
+
+LOCK TABLES `campaigns` WRITE;
+/*!40000 ALTER TABLE `campaigns` DISABLE KEYS */;
+/*!40000 ALTER TABLE `campaigns` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `classes`
+--
+
+DROP TABLE IF EXISTS `classes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `classes` (
+  `id` varchar(40) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `id_couse` varchar(45) NOT NULL,
+  `created_by` varchar(255) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `last_updated_date` datetime DEFAULT NULL,
+  `last_updated_by` varchar(255) DEFAULT NULL,
+  `version` bigint(20) NOT NULL,
+  `courses_id` varchar(255) NOT NULL,
+  `delected` bit(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_classes_courses1_idx` (`courses_id`),
+  CONSTRAINT `fk_classes_courses1` FOREIGN KEY (`courses_id`) REFERENCES `courses` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `classes`
+--
+
+LOCK TABLES `classes` WRITE;
+/*!40000 ALTER TABLE `classes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `classes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `courses`
+--
+
+DROP TABLE IF EXISTS `courses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `courses` (
+  `id` varchar(255) NOT NULL,
+  `created_by` varchar(255) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `last_updated_date` datetime DEFAULT NULL,
+  `last_updated_by` varchar(255) DEFAULT NULL,
+  `version` bigint(20) NOT NULL,
+  `end_day` datetime DEFAULT NULL,
+  `deleted` bit(1) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `start_day` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `courses`
+--
+
+LOCK TABLES `courses` WRITE;
+/*!40000 ALTER TABLE `courses` DISABLE KEYS */;
+INSERT INTO `courses` VALUES ('1',NULL,'2019-04-23 15:41:41','2019-04-23 15:41:41',NULL,1,'2019-04-25 00:00:00',_binary '\0','Course 5','2019-04-23 00:00:00'),('2',NULL,'2019-04-23 15:43:02','2019-04-23 15:43:02',NULL,1,'2019-04-30 00:00:00',_binary '\0','Course 6','2019-04-01 00:00:00'),('3',NULL,'2019-04-23 15:54:52','2019-04-23 15:54:52',NULL,1,'2019-04-30 00:00:00',_binary '\0','Course 7','2019-04-01 00:00:00');
+/*!40000 ALTER TABLE `courses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `leads`
+--
+
+DROP TABLE IF EXISTS `leads`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `leads` (
+  `id` varchar(20) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `phoneNumber` varchar(45) NOT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `source` varchar(45) DEFAULT NULL,
+  `idStatus` varchar(45) NOT NULL,
+  `created_by` varchar(255) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `last_updated_date` datetime DEFAULT NULL,
+  `last_updated_by` varchar(255) DEFAULT NULL,
+  `version` bigint(20) NOT NULL,
+  `delected` bit(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `foriengnKeyStatus_idx` (`idStatus`),
+  CONSTRAINT `foriengnKeyStatus` FOREIGN KEY (`idStatus`) REFERENCES `statuses` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `leads`
+--
+
+LOCK TABLES `leads` WRITE;
+/*!40000 ALTER TABLE `leads` DISABLE KEYS */;
+/*!40000 ALTER TABLE `leads` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `statuses`
+--
+
+DROP TABLE IF EXISTS `statuses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `statuses` (
+  `id` varchar(20) NOT NULL,
+  `nameStatus` varchar(45) NOT NULL,
+  `created_by` varchar(255) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `last_updated_date` datetime DEFAULT NULL,
+  `last_updated_by` varchar(255) DEFAULT NULL,
+  `version` bigint(20) NOT NULL,
+  `delected` bit(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `statuses`
+--
+
+LOCK TABLES `statuses` WRITE;
+/*!40000 ALTER TABLE `statuses` DISABLE KEYS */;
+/*!40000 ALTER TABLE `statuses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `students`
+--
+
+DROP TABLE IF EXISTS `students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `students` (
+  `id` varchar(36) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `birthday` date DEFAULT NULL,
+  `gender` varchar(10) NOT NULL,
+  `classes_id` varchar(40) NOT NULL,
+  `created_by` varchar(255) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `last_updated_date` datetime DEFAULT NULL,
+  `last_updated_by` varchar(255) DEFAULT NULL,
+  `version` bigint(20) NOT NULL,
+  `delected` bit(1) NOT NULL,
+  PRIMARY KEY (`id`,`classes_id`),
+  KEY `fk_students_classes1_idx` (`classes_id`),
+  CONSTRAINT `fk_students_classes1` FOREIGN KEY (`classes_id`) REFERENCES `classes` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `students`
+--
+
+LOCK TABLES `students` WRITE;
+/*!40000 ALTER TABLE `students` DISABLE KEYS */;
+/*!40000 ALTER TABLE `students` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-04-23 16:42:39
