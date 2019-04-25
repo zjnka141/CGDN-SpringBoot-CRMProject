a4071be86ea86955fa2c5fce995f685cff0d9fdf
Thang <sontana236@gmail.com>
2019-04-25 17:18:40 +0700

    Header, footer, update search, paging course

--- src/main/java/dn/codegym/crm/service/CourseService.java
+++ src/main/java/dn/codegym/crm/service/CourseService.java
@@ -2,9 +2,12 @@ package dn.codegym.crm.service;
 
 import dn.codegym.crm.dto.CourseDTO;
 import dn.codegym.crm.entity.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface CourseService {
-    Iterable<Course> findAllByDeletedIsFalse();
+    Page<Course> findAllByDeletedIsFalse(Pageable pageable);
+    Page<Course> findAllByNameContaining(String name, Pageable pageable);
     void create(CourseDTO courseDTO);
     void update(CourseDTO courseDTO);
     CourseDTO findById(String id);
