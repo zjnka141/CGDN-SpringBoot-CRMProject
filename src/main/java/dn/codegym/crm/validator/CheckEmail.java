c508b7c91129814fa626a27df4b742b3b62594f9
tranvannhut <nhuttran030794@gmail.com>
2019-06-20 11:33:52 +0700

    Update: 1.StudentDTO has attribute birthday has datatype String. I transform property  of birthday has  3 property int date ; int month; int year. 1.Create annotation CheckMail(description: Not allow  email is similar when we create a new student and update student) 2. edit interface of  create page  and edit page.

---
+++ src/main/java/dn/codegym/crm/validator/CheckEmail.java
@@ -0,0 +1,15 @@
+package dn.codegym.crm.validator;
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = CheckEmailValidator.class)
+@Target( { ElementType.METHOD, ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+
+public @interface CheckEmail {
+    String message() default "Please try again because your email  is exist!";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
